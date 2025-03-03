f56d1a7cfcf63a07d59ef1b2d792527c57caa66a
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-03 12:52:48 +0530

    Improved structure of API Error and Response

--- src/utils/APIError.js
+++ src/utils/APIError.js
@@ -1,5 +1,5 @@
 class APIError extends Error {
-  constructor(message, statusCode) {
+  constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
     this.timeStamp = new Date().toISOString();
--- src/utils/APIResponse.js
+++ src/utils/APIResponse.js
@@ -1,13 +1,15 @@
 class APIResponse {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, data) {
     this.message = message;
     this.statusCode = statusCode;
     this.timeStamp = new Date().toISOString();
+    this.data = data;
   }
 
   toJSON() {
     return {
       message: this.message,
+      data: this.data,
       statusCode: this.statusCode,
       timeStamp: this.timeStamp,
       success: true,
