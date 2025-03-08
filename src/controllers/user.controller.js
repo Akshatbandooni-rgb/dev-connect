6d6fe48ac538bbca35e2337bca03b43a6bd64a6c
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-08 23:57:58 +0530

    feat(user): add controller for fetching user connections with standardized response

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,38 @@
+const ConnectionRequest = require("../models/connectionRequest");
+const SchemaEnums = require("../constants/schema-values.enum");
+const ApiResponse = require("../utils/APIResponse");
+
+const getUserConnection = async (req, res, next) => {
+  try {
+    const currentLoggedInUser = req.loggedInUser;
+
+    const connections = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: currentLoggedInUser._id,
+          status: SchemaEnums.ConnectionStatus.ACCEPTED,
+        },
+        {
+          toUserId: currentLoggedInUser._id,
+          status: SchemaEnums.ConnectionStatus.ACCEPTED,
+        },
+      ],
+    })
+      .populate("fromUserId toUserId", "firstName lastName age gender")
+      .lean();
+
+    const successResponse = new ApiResponse(
+      "🔗 User connections fetched successfully",
+      200,
+      { connections }
+    ).toJSON();
+
+    res.status(200).json(successResponse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getUserConnection,
+};
