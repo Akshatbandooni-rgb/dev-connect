7158c75f53154b76b27dd94126c906757efb5d99
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-09 00:03:13 +0530

    refactor(user): remove unused imports in user router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const BlockList = require("../models/blockList");
-const ConnectionRequest = require("../models/connectionRequest");
-const SchemaEnums = require("../constants/schema-values.enum");
-const ApiResponse = require("../utils/APIResponse");
 const {
   getUserConnection,
   getUserRequests,
