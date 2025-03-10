19e255ec0af7aee9f80e7a0bf24c57ae86eea6b2
Akshat Bandooni <63807929+Akshatbandooni-rgb@users.noreply.github.com>
2025-03-10 23:45:44 +0530

    refactor(auth): streamline user authentication middleware for clarity

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,23 @@
 const jwt = require("jsonwebtoken");
 const Constants = require("../constants/constants");
 const User = require("../models/user");
+
+// Middleware to authenticate users
 const userAuthentication = async (req, res, next) => {
   try {
-    const token = req.cookies[Constants.TOKEN];
-    if (!token) {
-      throw new Error("Access Denied");
-    }
-    const decodedToken = jwt.verify(token, Constants.JWT_SECRET_KEY);
-    const { _id: userId } = decodedToken;
-    const user = await User.findById(userId);
-    if (!user) {
-      throw new Error("Token not valid");
-    }
-    req.loggedInUser = user;
+    const token = req.cookies[Constants.TOKEN]; // Get token from cookies
+    if (!token) throw new Error("Access Denied");
+
+    const decodedToken = jwt.verify(token, Constants.JWT_SECRET_KEY); // Verify token
+    const user = await User.findById(decodedToken._id); // Fetch user
+
+    if (!user) throw new Error("Token not valid");
+
+    req.loggedInUser = user; // Attach user to request
     next();
   } catch (error) {
-    return res.status(403).json({ message: error.message });
+    res.status(403).json({ message: error.message }); // Handle errors
   }
 };
 
-module.exports = {
-  userAuthentication,
-};
+module.exports = { userAuthentication };
